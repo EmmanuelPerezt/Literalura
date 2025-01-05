2cd951ff0d08d1912e98c33db44815785da43753
Your Name <emmanuel.pereztj@gmail.com>
2025-01-04 18:44:47 -0600

    add: various changes

--- src/main/java/com/biblioteca/DataAddedService.java
+++ src/main/java/com/biblioteca/DataAddedService.java
@@ -13,16 +13,14 @@ public class DataAddedService {
     private BookRepository bookRepository;
 
 
-
-
-
-
-
     public void addData () throws IOException, InterruptedException{
-
+        bookRepository.deleteAll();
         var request = new Httprequestcontroller();
         String res = request.sendRequest("https://gutendex.com/books/");
-        
+        //iterar sobre el json con jackson al res
+        //guardar en la base de datos
+
+
 
 
     }
